b812d190561c99d0e564a4d2eb441ad3c28d5a37
mastermark5 <mastermark51200@gmail.com>
2015-07-20 19:40:55 -0400

    Updated readme + documentation
    - Updated readme to be even MORE specific
    - Referenced demos in readme
    - Documented the "Hello world" demo

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Sigma is a tool to help people learn to program. It allows people who have never
 
 ### Details
 Sigma is a GUI for the Sigma Framework. The Sigma Framework allows you to dynamically create theoretical Java objects, put them together, and extract a full and runnable Java source file (.java). Both Sigma and the Sigma Framework are being developed in this project.
+
+Both Sigma and the Sigma Framework are written in Java. A Sigma script consists of a SigmaClass and any object or method in that class. Please see the demos (com.markaldrich.sigma.demo) to see how it works and use correct formatting.
--- src/com/markaldrich/sigma/demo/SigmaFrameworkHelloWorldDemo.java
+++ src/com/markaldrich/sigma/demo/SigmaFrameworkHelloWorldDemo.java
@@ -9,12 +9,18 @@ import com.markaldrich.sigma.framework.elements.*;
  */
 public class SigmaFrameworkHelloWorldDemo {
 	public static void main(String[] args) {
+		// A group of brackets denotes that anything done inside them belongs to the object above them.
+		// This is done to maintain neat-ish indentation.
+		
+		// Create a new SigmaScript.
 		SigmaScript script = new SigmaScript();
 		{
+			// Create a new method, call it main, set its attributes
 			SigmaClass main = new SigmaClass();
 			main.name = "Main";
 			main.access = SigmaAccessModifier.PUBLIC;
 			{
+				// Create a new global variable, set its name, type, etc. and add it to the main method
 				SigmaGlobalVariable helloWorldString = new SigmaGlobalVariable();
 				helloWorldString.name = "helloWorldString";
 				helloWorldString.type = "String";
@@ -23,6 +29,7 @@ public class SigmaFrameworkHelloWorldDemo {
 				helloWorldString.isStatic = true;
 				main.globalVariables.add(helloWorldString);
 				
+				// Create a new method
 				SigmaMethod mainMethod = new SigmaMethod();
 				mainMethod.access = SigmaAccessModifier.PUBLIC;
 				mainMethod.returnType = "void";
@@ -30,15 +37,19 @@ public class SigmaFrameworkHelloWorldDemo {
 				mainMethod.parameters.put("args", "String[]");
 				mainMethod.name = "main";
 				{
+					// Call a method
 					SigmaMethodCall s1 = new SigmaMethodCall();
 					s1.method = "System.out.println";
 					s1.parameters.add(helloWorldString);
 					mainMethod.statements.add(s1);
 				}
+				// Add mainMethod to the class
 				main.methods.add(mainMethod);
 			}
+			// Set the "Main" class as the main class in the script
 			script.mainClass = main;
 		}
+		// Print the generated source code
 		System.out.println(script.getSource());
 		
 	}
